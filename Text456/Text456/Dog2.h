537f51fc0d434e33ddabc83354de15efa71587ec
kuailededoubi <HZZDL@bogon>
2016-10-10 11:36:07 +0800

    第二次添加Dog

---
+++ Text456/Text456/Dog2.h
@@ -0,0 +1,13 @@
+//
+//  Dog2.h
+//  Text456
+//
+//  Created by HZZDL on 16/10/10.
+//  Copyright © 2016年 HZZDL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog2 : NSObject
+
+@end
---
+++ Text456/Text456/Dog2.m
@@ -0,0 +1,13 @@
+//
+//  Dog2.m
+//  Text456
+//
+//  Created by HZZDL on 16/10/10.
+//  Copyright © 2016年 HZZDL. All rights reserved.
+//
+
+#import "Dog2.h"
+
+@implementation Dog2
+
+@end
