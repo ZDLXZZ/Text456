f610db3119f61508d6015d10660767fe1c825ce2
kuailededoubi <HZZDL@bogon>
2016-10-10 11:11:27 +0800

    加入Person类

---
+++ Text456/Text456/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Text456
+//
+//  Created by HZZDL on 16/10/10.
+//  Copyright © 2016年 HZZDL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Text456/Text456/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Text456
+//
+//  Created by HZZDL on 16/10/10.
+//  Copyright © 2016年 HZZDL. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
